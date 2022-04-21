9a508567d2f6c5ae594625ee32cc921a6fed8c77
Ronin <rlombardino44@gmail.com>
2022-04-21 11:53:47 -0700

    added ability to add bonus to dice rolls in functions

--- DiceRollers.js
+++ DiceRollers.js
@@ -1,34 +1,49 @@
 //1d4
-const DFour = () => {
-  return Math.floor(Math.random() * 4 + 1)
+const DFour = (Bonus) => {
+  const roll = Math.floor(Math.random() * 4 + 1)
+  let total = roll + (Bonus || 0)
+  return total
 }
 
 //1d6
-const DSix = () => {
-  return Math.floor(Math.random() * 6 + 1)
+const DSix = (Bonus) => {
+  const roll = Math.floor(Math.random() * 6 + 1)
+  let total = roll + (Bonus || 0)
+  return total
 }
 
 //1d8
-const DEight = () => {
-  return Math.floor(Math.random() * 8 + 1)
+const DEight = (Bonus) => {
+  const roll = Math.floor(Math.random() * 8 + 1)
+  let total = roll + (Bonus || 0)
+  return total
 }
 
 //1d10
-const DTen = () => {
-  return Math.floor(Math.random() * 10 + 1)
+const DTen = (Bonus) => {
+  const roll = Math.floor(Math.random() * 10 + 1)
+  let total = roll + (Bonus || 0)
+  return total
 }
 
 //1d12
-const DTwelve = () => {
-  return Math.floor(Math.random() * 12 + 1)
+const DTwelve = (Bonus) => {
+  const roll = Math.floor(Math.random() * 12 + 1)
+  let total = roll + (Bonus || 0)
+  return total
 }
 
 //1d20
-const DTwenty = () => {
-  return Math.floor(Math.random() * 20 + 1)
+const DTwenty = (Bonus) => {
+  const roll = Math.floor(Math.random() * 20 + 1)
+  let total = roll + (Bonus || 0)
+  return total
 }
 
 //1d100
-const DOneHundred = () => {
-  return Math.floor(Math.random() * 100 + 1)
+const DOneHundred = (Bonus) => {
+  const roll = Math.floor(Math.random() * 100 + 1)
+  let total = roll + (Bonus || 0)
+  return total
 }
+
